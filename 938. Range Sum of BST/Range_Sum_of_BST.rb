9e963f185a4c2665c607f2ec349e7584beff2086
Tony <tony1996_wang@hotmail.com>
2020-08-05 23:08:58 -0700

    Added Range Sum of BST

---
+++ 938. Range Sum of BST/Range_Sum_of_BST.rb
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} l
+# @param {Integer} r
+# @return {Integer}
+def range_sum_bst(root, l, r)
+    
+    return 0 if root == nil
+    
+    if root.val < l
+        return range_sum_bst(root.right, l, r)
+    elsif root.val > r
+        return range_sum_bst(root.left, l, r)
+    else 
+        return root.val + range_sum_bst(root.left, l, r) + range_sum_bst(root.right, l, r)
+    end
+    
+end
